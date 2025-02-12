6a1bb93e58d7e79ac55bc1929a2151a44894e30d
daeheekim <dae4805@naver.com>
2025-02-12 23:41:07 +0900

    Update Data

--- src/scene/3.InfoScene/section/ProfileSection.tsx
+++ src/scene/3.InfoScene/section/ProfileSection.tsx
@@ -9,20 +9,18 @@ const ProfileSection = () => {
                 <S.ProfileBorder/>
                 <S.ProfileImg src='./scene/3/profile/profileImg.png' alt='profileImg'/>
             </S.ProfileContainer>
-            <S.ProfileGreeting>
-                항상 즐겁게 개발하는<br/>
-                개발자 김대희입니다!<br/>
-            </S.ProfileGreeting>
-            <S.ProfileDescription>
+                <S.ProfileGreeting>
+                    안녕하세요!<br/>
+                    개발자 김대희입니다.<br/>
+                </S.ProfileGreeting>
+                <S.ProfileDescription>
                 <ul>
                     <li>
-                        항상 책임감을 가지고 임하는 성격입니다.
+                        좋은 코드를 위해서 동료들과
+                        <br/>의견을 나누는 것을 좋아합니다.
                     </li>
                     <li>
-                        항상 ‘왜?’라는 의문을 가지고 일을 하고 있습니다.
-                    </li>
-                    <li>
-                        항상 긍정적인 방향으로 생각하고 구성원들과 원활한 소통을 하고 있습니다.
+                        좋은 서비스는 동료들과 함께 <br/>이뤄지는 것이라고 생각합니다.<br/>
                     </li>
                 </ul>
             </S.ProfileDescription>
